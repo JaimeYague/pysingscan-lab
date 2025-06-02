8446acffc79fdfa892f2a22bdfc9ad1b8270203c
elMestre <carmesce@gmail.com>
2025-06-02 09:05:21 +0200

    pyperclip detection

--- cliente.py
+++ cliente.py
@@ -1,7 +1,11 @@
 from pyperadaptor import pyperclip, waitForNewPaste
 from servidor import servidor
 
+if not pyperclip.is_available():
+    print("The system clipboard is not supported.")
+    exit()
 
+print("PysingScan listening clipboard...")
 while True:
     txt = waitForNewPaste()
     print(txt)
